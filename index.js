ddccc38459f182db77b582de704658278fc86c8e
Sabaris <sabaris.elango@gmail.com>
2024-05-06 16:40:49 +0530

    Create a unique and sorted array before bst

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function Node(data) {
-  let data = data;
   let left = null;
   let right = null;
 
   return { data, left, right };
 }
+
+function buildTree(array) {
+  array = array.filter((value, index) => {
+    return array.indexOf(value) === index;
+  });
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  return array;
+}
+
+array = [1, 2, 10, 4, 5, 1, 4, 7];
+console.log(buildTree(array));
